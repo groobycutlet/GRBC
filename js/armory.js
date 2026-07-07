9311960ddb014fbad5a3fa3075280189bd8ecd42
Grooby <grooby_cutlet@proton.me>
2026-07-07 13:23:54 +0100

    Create armory.js

---
+++ js/armory.js
@@ -0,0 +1,8 @@
+/*===============================================================
+    G.R.B.C. Armory
+    Version 1.0
+
+    Reserved for future Armory-specific JavaScript.
+================================================================*/
+
+// Intentionally left empty.
