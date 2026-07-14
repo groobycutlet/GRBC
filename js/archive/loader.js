d6b977764a91d62788722e1da4665bd85f63a3c7
Grooby <grooby_cutlet@proton.me>
2026-07-14 14:21:20 +0100

    Create loader.js

---
+++ js/archive/loader.js
@@ -0,0 +1,66 @@
+/*==============================================================
+    G.R.B.C. CENTRAL INTELLIGENCE ARCHIVE
+
+    loader.js
+
+    Responsible ONLY for loading JSON collections.
+
+==============================================================*/
+
+"use strict";
+
+const ArchiveLoader = {
+
+    /*==========================================================
+        LOAD COLLECTION
+    ==========================================================*/
+
+    async load(path){
+
+        try{
+
+            const response = await fetch(path);
+
+            if(!response.ok){
+
+                throw new Error(
+
+                    "Unable to load " + path
+
+                );
+
+            }
+
+            const json = await response.json();
+
+            Archive.collectionName = json.collection;
+
+            Archive.documents = json.documents;
+
+            Archive.filtered = [];
+
+            document.getElementById(
+
+                "collectionTitle"
+
+            ).textContent = json.collection;
+
+            ArchiveUI.showWelcome();
+
+        }
+
+        catch(error){
+
+            console.error(error);
+
+            ArchiveUI.showError(
+
+                "Unable to load archive collection."
+
+            );
+
+        }
+
+    }
+
+};
